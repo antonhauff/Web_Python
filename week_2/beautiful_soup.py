2f6ba96cfdb240c84163057fe76d9f16c6f5891e
antonhauff <antonhauff@yandex.ru>
2020-07-01 23:39:41 +0300

    Fixed methods

--- week_2/beautiful_soup.py
+++ week_2/beautiful_soup.py
@@ -1,65 +1,42 @@
+from bs4 import BeautifulSoup
 import unittest
 
-from wikistat2 import build_bridge, get_statistics
 
-STATISTICS = {
-    'Artificial_intelligence': [8, 19, 13, 198],
-    'Binyamina_train_station_suicide_bombing': [1, 3, 6, 21],
-    'Brain': [19, 5, 25, 11],
-    'Haifa_bus_16_suicide_bombing': [1, 4, 15, 23],
-    'Hidamari_no_Ki': [1, 5, 5, 35],
-    'IBM': [13, 3, 21, 33],
-    'Iron_Age': [4, 8, 15, 22],
-    'London': [53, 16, 31, 125],
-    'Mei_Kurokawa': [1, 1, 2, 7],
-    'PlayStation_3': [13, 5, 14, 148],
-    'Python_(programming_language)': [2, 5, 17, 41],
-    'Second_Intifada': [9, 13, 14, 84],
-    'Stone_Age': [13, 10, 12, 40],
-    'The_New_York_Times': [5, 9, 8, 42],
-    'Wild_Arms_(video_game)': [3, 3, 10, 27],
-    'Woolwich': [15, 9, 19, 38]}
-
-TESTCASES = (
-    ('wiki/', 'Stone_Age', 'Python_(programming_language)',
-     ['Stone_Age', 'Brain', 'Artificial_intelligence', 'Python_(programming_language)']),
-
-    ('wiki/', 'The_New_York_Times', 'Stone_Age',
-     ['The_New_York_Times', 'London', 'Woolwich', 'Iron_Age', 'Stone_Age']),
-
-    ('wiki/', 'Artificial_intelligence', 'Mei_Kurokawa',
-     ['Artificial_intelligence', 'IBM', 'PlayStation_3', 'Wild_Arms_(video_game)',
-      'Hidamari_no_Ki', 'Mei_Kurokawa']),
-
-    ('wiki/', 'The_New_York_Times', "Binyamina_train_station_suicide_bombing",
-     ['The_New_York_Times', 'Second_Intifada', 'Haifa_bus_16_suicide_bombing',
-      'Binyamina_train_station_suicide_bombing']),
-
-    ('wiki/', 'Stone_Age', 'Stone_Age',
-     ['Stone_Age', ]),
-)
+def parse(path_to_file):
+    with open(path_to_file, encoding='utf8') as html_file:
+        soup = BeautifulSoup(html_file.read(), 'lxml').find(id='bodyContent')
+        imgs = len([img for img in soup.find_all('img', width=True) if int(img['width']) >= 200])
+        headers = len([header for header in soup.find_all(('h1', 'h2', 'h3', 'h4', 'h5', 'h6'))
+                       if header.text[:1] in ('E', 'T', 'C')])
+        linkslen = 0
+        for tag in soup.recursiveChildGenerator():
+            if tag.name == 'a':
+                cnt = 1
+                next_tags = tag.find_next_siblings()
+                if next_tags:
+                    for i in next_tags:
+                        if i.name == 'a':
+                            cnt += 1
+                        else:
+                            break
+                    if linkslen < cnt:
+                        linkslen = cnt
+        lists = sum(
+            1 for tag in soup.find_all(['ol', 'ul']) if not tag.find_parent(['ol', 'ul']))
+        return [imgs, headers, linkslen, lists]
 
 
-class TestBuildBrige(unittest.TestCase):
-    def test_build_bridge(self):
-        for path, start_page, end_page, expected in TESTCASES:
-            with self.subTest(path=path,
-                              start_page=start_page,
-                              end_page=end_page,
-                              expected=expected):
-                result = build_bridge(path, start_page, end_page)
-                self.assertEqual(result, expected)
-
-
-class TestGetStatistics(unittest.TestCase):
-    def test_build_bridge(self):
-        for path, start_page, end_page, expected in TESTCASES:
-            with self.subTest(path=path,
-                              start_page=start_page,
-                              end_page=end_page,
-                              expected=expected):
-                result = get_statistics(path, start_page, end_page)
-                self.assertEqual(result, {page: STATISTICS[page] for page in expected})
+class TestParse(unittest.TestCase):
+    def test_parse(self):
+        test_cases = (
+            ('wiki/Stone_Age', [13, 10, 12, 40]),
+            ('wiki/Brain', [19, 5, 25, 11]),
+            ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
+            ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
+            ('wiki/Spectrogram', [1, 2, 4, 7]),)
+        for path, expected in test_cases:
+            with self.subTest(path=path, expected=expected):
+                self.assertEqual(parse(path), expected)
 
 
 if __name__ == '__main__':
